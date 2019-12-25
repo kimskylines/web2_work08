2261c705d5ae22f1509bb45d6f27b89f71f281ef
kimskylines <kimskylines@gmail.com>
2019-12-25 14:27:40 +0800

    111

--- web2 2016011479 金鼎龙 2016011501王得润/js/dateCategory.js
+++ web2 2016011479 金鼎龙 2016011501王得润/js/dateCategory.js
@@ -30,16 +30,3 @@ setInterval(function(){
 	img6.src="image/"+_date[5]+".png";
 }, 1000);
 
-var href = window.location.href ;
-console.log(href);
-function parseURL(href){
-    var href = href.split("?")[1];
-    var para = href.split("&");
-    var len = para.length;
-    var res = {};
-    arr = para[0].split("=");
-    //console.log(arr[1]);
-    alert("用户"+arr[1]+"登录成功");
-  	return arr[1];
-}
-parseURL(href);
--- web2 2016011479 金鼎龙 2016011501王得润/js/dateIndex.js
+++ web2 2016011479 金鼎龙 2016011501王得润/js/dateIndex.js
@@ -29,17 +29,3 @@ setInterval(function(){
 	var img6 = document.getElementById("img6");
 	img6.src="image/"+_date[5]+".png";
 }, 1000);
-
-var href = window.location.href ;
-console.log(href);
-function parseURL(href){
-    var href = href.split("?")[1];
-    var para = href.split("&");
-    var len = para.length;
-    var res = {};
-    arr = para[0].split("=");
-    //console.log(arr[1]);
-    alert("用户"+arr[1]+"登录成功");
-  	return arr[1];
-}
-parseURL(href);
